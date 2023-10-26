dfa602821e46b07b88cf7fbb40c540fc949d4d12
GustavoAntunes-dev <gustavo.eng8@hotmail.com>
2023-10-26 19:13:10 -0300

    RodagemSuperior

--- js/main.js
+++ js/main.js
@@ -35,3 +35,22 @@ window.addEventListener("load", () => {
 
 
 /* =========== Rodagem superior =========== */
+const scrollTop = document.querySelector(".scroll-top");
+
+scrollTop.addEventListener("click", () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", (e) => {
+  const scrollHeight = window.scrollY;
+
+  if (scrollHeight > 300) {
+    scrollTop.classList.add("show");
+  } else {
+    scrollTop.classList.remove("show");
+  }
+});
